7c9ef40237b5a4497469303eec099b7ee038e9fb
Leandro Fantin <lfantin@mac-lfantin.local>
2014-07-15 17:55:27 -0300

    first commit

---
+++ EjemploWizard/ViewControllerStep2.m
@@ -0,0 +1,38 @@
+//
+//  ViewControllerStep2.m
+//  EjemploWizard
+//
+//  Created by Leandro Fantin on 15/07/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import "ViewControllerStep2.h"
+
+@interface ViewControllerStep2 ()
+
+@end
+
+@implementation ViewControllerStep2
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
