f6ba87ed1c3b866fa074e3545d5d426f5d27d15e
Leandro Fantin <lfantin@mac-lfantin.local>
2014-07-15 14:16:12 -0300

    Initial Commit

---
+++ EjemploWizard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EjemploWizard
+//
+//  Created by Leandro Fantin on 15/07/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EjemploWizard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EjemploWizard
+//
+//  Created by Leandro Fantin on 15/07/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
